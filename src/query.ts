5c2e84ba4b54b6b4ddebcdb7128cffab1fd9a1cd
mayk.akifovski <mayk.akifovski@gmail.com>
2019-06-21 13:45:12 +0200

    scripts for communication with network

---
+++ src/query.ts
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {FileSystemWallet, Gateway} from 'fabric-network';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const ccpPath = path.resolve(__dirname, '..', '..', 'landregister-network', 'basic-network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+async function main() {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('notary_1');
+        if (!userExists) {
+            console.log('An identity for the user "notary_1" does not exist in the wallet');
+            console.log('Run the registerUser.ts application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, {wallet, identity: 'notary_1', discovery: {enabled: false}});
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('landregister');
+
+        // Evaluate the specified transaction.
+        // queryLandRegister
+        const result = await contract.evaluateTransaction('queryLandRegister', 'landRegister-Eutin-Malente-3323');
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
